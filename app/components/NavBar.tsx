e996e38d92ddf72094240f2ef2d1f9f401290db1
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2024-10-09 11:20:49 +0200

    changes

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,8 +4,10 @@ import './NavBar.css';
 
 const NavBar = () => {
   return (
-      <nav className='nav--bar'>
+    <nav className='nav--bar'>
+      <a href='/'>
         <Image src="/logo.png" alt="logo" width={100} height={100} priority/>
+      </a>
         <ul>  
             <li>Home</li>
             <li>Projects</li>
