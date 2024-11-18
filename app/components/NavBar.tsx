d8738eeff0416aa5cbf96c5c6d1b793a02e381ef
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2024-11-18 23:18:40 +0100

    upday

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image';
 import './NavBar.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const NavBar = () => {
   return (
@@ -8,12 +9,17 @@ const NavBar = () => {
       <a href='/'>
         <Image src="/logo.png" alt="logo" width={100} height={100} priority/>
       </a>
-        <ul>  
-            <li>Home</li>
-            <li>Projects</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
+      <ul>  
+          <li>Home</li>
+          <li>Projects</li>
+          <li>About</li>
+          <li>Contact</li>
+      </ul>
+      <ul>
+        <li><a href="https://www.instagram.com" target="_blank"><i className="fab fa-instagram"></i></a></li>
+        <li><a href="https://www.linkedin.com" target="_blank"><i className="fab fa-linkedin"></i></a></li>
+        <li><a href="https://www.github.com" target="_blank"><i className="fab fa-github"></i></a></li>
+      </ul>
     </nav>
   )
 }
